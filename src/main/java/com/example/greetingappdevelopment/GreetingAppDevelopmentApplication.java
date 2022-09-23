1a3bd53a3395f7567768126ee071b68b1358fd82
champ09D <darshanmhatre709@gmail.com>
2022-09-23 22:06:31 +0530

    Added : Using Greeting  Controller File Case Add

---
+++ src/main/java/com/example/greetingappdevelopment/GreetingAppDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.greetingappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppDevelopmentApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
